51ffc29228c090eef8f313c87d26b0c10189d61b
Capevace <capevace@gmail.com>
2017-01-19 16:49:18 +0100

    Switched to own regex ip parsing and fixed indent

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
 const express = require('express');
 const app = express();
-const ipRegex = require('ip-regex');
 
 app.set('port', (process.env.PORT || 5000));
 
 app.all('*', (req, res) => {
-    const ip = req.ip.match(ipRegex())[0];
+  const ipMatches = req.ip.match(/(::ffff:)?(.+)/);
+  let ip = ipMatches[0];
 
-    // Match the content of the brackets in the user agent.
-    // We take the one at index 1 because that one doesn't include the brackets itself.
-    const operatingSystem = req.get('User-Agent').match(/\((.+?)\)/)[1];
+  // If the second match is not undefined, we can use the thrird match, because its an ipv6 address
+  if (ipMatches[1]) {
+    ip = ipMatches[2];
+  }
 
-    // Match the first language string (first before the comma).
-    const language = req.get('Accept-Language').match(/(.+?),/)[1];
+  // Match the content of the brackets in the user agent.
+  // We take the one at index 1 because that one doesn't include the brackets itself.
+  const operatingSystem = req.get('User-Agent').match(/\((.+?)\)/)[1];
 
-    res.json({
-      ipaddress: ip,
-      language,
-      software: operatingSystem,
-    });
+  // Match the first language string (first before the comma).
+  const language = req.get('Accept-Language').match(/(.+?),/)[1];
+
+  res.json({
+    ipaddress: ip,
+    language,
+    software: operatingSystem,
+  });
 });
 
 app.listen(app.get('port'), () => {
